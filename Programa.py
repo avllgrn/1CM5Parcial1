61f41231f467257f9d90d298b3f04d5dd17f70ee
avllgrn <avillagran@ipn.mx>
2026-09-07 13:49:53 -0600

    Ventana con elementos en malla

--- Programa.py
+++ Programa.py
@@ -8,20 +8,34 @@ if __name__== '__main__':
     Ventana = tkinter.Tk()
     Ventana.title('Mi ventana')
 
-    # Se construye elemento
-    CajaDeTexto = tkinter.Entry()
-    # Se empaqueta elemento
-    CajaDeTexto.pack()
+    # Se construye elementos
+    Etiqueta1 = tkinter.Label(text='A')
+    Caja1 = tkinter.Entry()
+    # Se agregan elementos
+    Etiqueta1.grid(row=0, column=0)
+    Caja1.grid(row=0, column=1)
 
-    # Se construye elemento
-    Etiqueta = tkinter.Label(text='Etiqueta')
-    # Se empaqueta elemento
-    Etiqueta.pack()
+    # Se construye elementos
+    Etiqueta2 = tkinter.Label(text='B')
+    Caja2 = tkinter.Entry()
+    # Se agregan elementos
+    Etiqueta2.grid(row=1, column=0)
+    Caja2.grid(row=1, column=1)
 
-    # Se construye elemento
-    Boton = tkinter.Button(text='Presiona Botón')
-    # Se empaqueta elemento
-    Boton.pack()
+    # Se construye elementos
+    Etiqueta3 = tkinter.Label(text='C')
+    Caja3 = tkinter.Entry()
+    # Se agregan elementos
+    Etiqueta3.grid(row=2, column=0)
+    Caja3.grid(row=2, column=1)
+
+    # Se construye elementos
+    Boton1 = tkinter.Button(text='Suma')
+    Boton2 = tkinter.Button(text='Limpia')
+    # Se agregan elementos
+    Boton1.grid(row=3, column=0)
+    Boton2.grid(row=3, column=1, sticky='nsew')
+
 
     # Se muestra la vantana
     Ventana.mainloop()
